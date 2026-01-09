f1b60cc96259aa8c80310ef2a6fa68dd302bd4cd
Joby Martin <jobymartin4th@gmail.com>
2026-01-09 17:15:06 -0500

    Add system test for creating an invalid location

--- spec/system/location_weather_spec.rb
+++ spec/system/location_weather_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe "Viewing a location without coordinates", type: :system do
     expect(page).to have_content("Weather data is unavailable for this location")
     expect(page).to have_content("7 Day Forecast for Nowhereville")
   end
+
+  it "shows an alert if a location cannot be geocoded" do
+    # Stub the API call to return no results
+    stub_request(:get, "https://geocoding-api.open-meteo.com/v1/search?count=1&format=json&language=en&name=Nowhereville")
+      .to_return(status: 200, body: { results: [] }.to_json)
+
+    visit new_location_path
+
+    fill_in "Name", with: "Nowhereville"
+    click_button "Create Location"
+
+    expect(page).to have_content("Could not find coordinates for this location.")
+  end
 end
