605a05dd026bf51f2f90aa24d9cb12f6f3c9e990
Joby Martin <jobymartin4th@gmail.com>
2026-01-13 14:37:02 -0500

    Refactor system tests for model-based geocode failure handling

--- spec/system/location_weather_spec.rb
+++ spec/system/location_weather_spec.rb
@@ -2,16 +2,8 @@ require "rails_helper"
 
 RSpec.describe "Viewing a location without coordinates", type: :system do
   before { driven_by(:rack_test) } # run without selenium due to more SSL errors
-  it "shows a flash alert and does not crash" do
-    location = Location.create!(name: "Nowhereville", latitude: nil, longitude: nil)
 
-    visit location_path(location)
-
-    expect(page).to have_content("Weather data is unavailable for this location")
-    expect(page).to have_content("7 Day Forecast for Nowhereville")
-  end
-
-  it "shows an alert if a location cannot be geocoded" do
+  it "shows an error and reveals latitude/longitude fields when a location cannot be geocoded" do
     # Stub the API call to return no results
     stub_request(:get, "https://geocoding-api.open-meteo.com/v1/search?count=1&format=json&language=en&name=Nowhereville")
       .to_return(status: 200, body: { results: [] }.to_json)
@@ -21,6 +13,11 @@ RSpec.describe "Viewing a location without coordinates", type: :system do
     fill_in "Name", with: "Nowhereville"
     click_button "Create Location"
 
-    expect(page).to have_content("Could not find coordinates for this location.")
+    # Error is displayed
+    expect(page).to have_content('Geocoding failed. Please enter latitude and longitude manually.')
+
+    # Lat/lon inputs are visible now
+    expect(page).to have_field('Latitude')
+    expect(page).to have_field('Longitude')
   end
 end
