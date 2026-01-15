5b16defe8e4c1a50e55be742c6e43b65863a8316
Joby Martin <jobymartin4th@gmail.com>
2026-01-15 16:50:03 -0500

    Fix chart rendering for negative temperatures

--- app/services/chart_service.rb
+++ app/services/chart_service.rb
@@ -16,8 +16,16 @@ class ChartService
     chxl = "0:|#{dates.join('|')}"
     chd = "t:#{highs.join(',')}|#{lows.join(',')}"
     chco = "FF0000,0000FF"
+    chxs = "1,000000"
+
+    # Find min and max temperature for proper chart rendering
+    temps = highs + lows
+    min_temp = temps.min
+    max_temp = temps.max
+    chds = "#{(min_temp - 5).round(0)},#{(max_temp + 5).round(0)}"
+    chxr = "1,#{(min_temp - 5).round(0)},#{(max_temp + 5).round(0)}"
 
     # Return the final URL
-    "#{base_url}?cht=lc&chs=700x300&chxt=x,y&chxl=#{chxl}&chd=#{chd}&chco=#{chco}&chg=14.28,20"
+    "#{base_url}?cht=lc&chs=700x300&chxt=x,y&chxl=#{chxl}&chd=#{chd}&chco=#{chco}&chg=14.28,20&chds=#{chds}&chxr=#{chxr}&chxs=#{chxs}"
   end
 end
