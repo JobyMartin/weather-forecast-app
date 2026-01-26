7f142f100a10671122c34fe56b33f1bbd915507e
Joby Martin <jobymartin4th@gmail.com>
2026-01-26 16:08:54 -0500

    Move Location geocoding from validation to before_validation

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,18 +1,22 @@
 class Location < ApplicationRecord
-  validate :must_be_geocoded
+  before_validation :geocode_if_needed
+  validate :ensure_geocoded
 
   private
 
-  def must_be_geocoded
+  def geocode_if_needed
     return if latitude.present? && longitude.present?
 
     result = GeocodeService.geocoder(name)
-
-    if result.nil?
-      errors.add(:base, "Geocoding failed. Please enter latitude and longitude manually.")
-    else
+    if result.present?
       self.latitude = result[:latitude]
       self.longitude = result[:longitude]
+    end
+  end
+
+  def ensure_geocoded
+    if latitude.blank? || longitude.blank?
+      errors.add(:base, "Geocoding failed. Please enter latitude and longitude manually.")
     end
   end
 end
